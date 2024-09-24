04801677f4c3c287679a58545586fbb2e083386c
GeMy119 <mahmoud.gamal1191998@gmail.com>
2024-09-24 14:26:32 +0300

    update api link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export interface Environment {
 }
 export const environment: Environment = {
   production: true,
-  apiUrl: "https://api.suadiestalamsurvices.com"
+  apiUrl: "https://api.suadiservicesestalam.com"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export interface Environment {
 }
 export const environment: Environment = {
   production: false,
-  apiUrl: "https://api.suadiestalamsurvices.com"
+  apiUrl: "https://api.suadiservicesestalam.com"
 };
 /*
  * For easier debugging in development mode, you can import the following file
