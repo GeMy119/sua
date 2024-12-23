a93402c757cf52d8fe49e4701a568e0d86dec32d
GeMy119 <mahmoud.gamal1191998@gmail.com>
2024-12-23 20:24:03 +0200

    update title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'الخدمات الالكترونيةالاستعلام العام برقم المعامله';
+  title = 'الخدمات الالكترونية الاستعلام العام برقم المعامله';
 }
