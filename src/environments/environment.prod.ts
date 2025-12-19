273a6d295e97fbc6f978a0a78ae1d60063c16504
GeMy119 <mahmoud.gamal1191998@gmail.com>
2025-12-19 21:43:53 +0200

    edit environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,11 @@ export interface Environment {
   production: boolean,
   apiUrl: string
 }
+// export const environment: Environment = {
+//   production: true,
+//   apiUrl: "https://api.servicesestalam.com"
+// };
 export const environment: Environment = {
-  production: true,
+  production: false,
   apiUrl: "https://api.servicesestalam.com"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,10 @@ export interface Environment {
   production: boolean,
   apiUrl: string
 }
+// export const environment: Environment = {
+//   production: false,
+//   apiUrl: "https://api.servicesestalam.com"
+// };
 export const environment: Environment = {
   production: false,
   apiUrl: "https://api.servicesestalam.com"
